e7eebcb2eb21722260e13d80dec67ff3417be272
mohammedegila <mohammedegila@gmail.com>
2021-05-02 02:46:21 +0200

    Add (model&migrations) for [categry, comment and post]

---
+++ database/migrations/2021_05_02_004107_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('cat_id');
+            $table->string('title');
+            $table->string('thumb');
+            $table->string('full_img');
+            $table->text('details');
+            $table->string('tags');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('cat_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
